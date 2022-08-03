a7d9f69aef9efa539114afcc6a25e3f72731fcc3
alaminhossen899 <alaminhossen899@gmail.com>
2022-08-03 23:58:01 +0600

    [feat]:added obect and show in contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,31 @@
-import React from "react";
+import {useEffect,useState} from "react";
 
 export default function Contact() {
+    const [data,setDataa] = useState(null);
+
+    useEffect(()=>{
+        const fetchData =() => {
+          const result = {
+              data:{
+                  name:"Al-Amin",
+                  email:"alamin@appnap.io",
+                  mob:"01758845299",
+                  bg:"AB+",
+
+              }
+          };
+          setDataa(result.data)
+        }
+        fetchData()
+    },[]);
+
     return(
         <div>
             <h1>Contact</h1>
+            <hr/>
+            <h1>Name : {data?.name}</h1>
+            <h1>Mobile : {data?.mob}</h1>
+            <h1>Blood Group : {data?.bg}</h1>
         </div>
     );
 }
